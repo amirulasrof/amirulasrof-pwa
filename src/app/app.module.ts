e283597d655febf1d29353720b53185e0c5dc2f7
amirulasrof <acop95@gmail.com>
2018-09-20 20:36:08 +0800

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,33 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+//import { ListPage } from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+//import { StatusBar } from '@ionic-native/status-bar';
+//import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    //ListPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    //mode ios
+    IonicModule.forRoot(MyApp, {
+      mode: "ios",
+    }),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    //ListPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
+    //StatusBar,
+    //SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
